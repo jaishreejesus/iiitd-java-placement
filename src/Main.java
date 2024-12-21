de9545eab5adbecc55fd7713b768f6dbf0d3e8dd
Arihant Gupta <72906949+GuptajiRocks@users.noreply.github.com>
2024-12-21 21:32:41 +0530

    building student class

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,47 @@
 import java.util.*;
 public class Main {
     static Scanner sc = new Scanner(System.in);
+    static Student testStudent = new Student();
 
     public static void mainApplication() {
+        while (true) {
+            System.out.println("Choose the mode you wish to enter: ");
+            System.out.println("1) Enter as Student Mode");
+            System.out.println("2) Enter as Company Mode");
+            System.out.println("3) Enter as Placement Cell Mode");
+            System.out.println("4) Exit back to main menu)");
+
+            int mainAppchoice = sc.nextInt();
+            if (mainAppchoice == 1) {
+
+            } else if (mainAppchoice == 2) {
+
+            } else if (mainAppchoice == 3) {
+
+            } else if (mainAppchoice == 4) {
+                break;
+            } else {
+                System.out.println("Invalid choice");
+                continue;
+            }
+        }
 
 
     }
 
     public static void main(String[] args) {
-        System.out.println("Welcome to FutureBuilder: ");
-        System.out.println("1: Enter the application");
-        System.out.println("2. Exit the application");
-        int first_choice = sc.nextInt();
-        if(first_choice == 1) {
-            mainApplication();
-        } else if (first_choice == 2) {
-            System.out.println("Thank you for using FutureBuilder");
-            System.exit(0);
+
+        while (true) {
+            System.out.println("Welcome to FutureBuilder: ");
+            System.out.println("1: Enter the application");
+            System.out.println("2. Exit the application");
+            int first_choice = sc.nextInt();
+            if (first_choice == 1) {
+                mainApplication();
+            } else if (first_choice == 2) {
+                System.out.println("Thank you for using FutureBuilder");
+                System.exit(0);
+            }
         }
     }
 }
--- src/Student.java
+++ src/Student.java
@@ -1,5 +1,35 @@
-public class Student {
-    public void bulla() {
-        System.out.println("Hello World");
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+class StudentDetails {
+    String name;
+    String rollNo;
+    double cgpa;
+    String branch;
+
+    String curentStatus = null;
+
+
+    StudentDetails(String name, String rollNo, double cgpa, String branch) {
+        this.name = name;
+        this.rollNo = rollNo;
+        this.cgpa = cgpa;
+        this.branch = branch;
     }
+
+    String getCurentStatus() {
+        return curentStatus;
+    }
+
+}
+
+public class Student {
+    public static String returnDateTime() {
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        return now.format(formatter);
+    }
+
+
+
 }
