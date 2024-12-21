c4265f2605c752ec1cd7b9df28f578c93abe5c0c
Arihant Gupta <72906949+GuptajiRocks@users.noreply.github.com>
2024-12-21 18:20:05 +0530

    started building

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,23 @@
 import java.util.*;
 public class Main {
+    static Scanner sc = new Scanner(System.in);
+
+    public static void mainApplication() {
+
+
+    }
+
     public static void main(String[] args) {
-        Student s1 = new Student();
-        s1.bulla();
+        System.out.println("Welcome to FutureBuilder: ");
+        System.out.println("1: Enter the application");
+        System.out.println("2. Exit the application");
+        int first_choice = sc.nextInt();
+        if(first_choice == 1) {
+            mainApplication();
+        } else if (first_choice == 2) {
+            System.out.println("Thank you for using FutureBuilder");
+            System.exit(0);
+        }
     }
 }
 
